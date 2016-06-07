6dde983f38cfcceefaacbb7a238d8cca48e721ee
Anna Natorilla <annagaiel@gmail.com>
2016-06-06 19:05:45 -0700

    Add Quotes class with say method

---
+++ lib/inspired.rb
@@ -0,0 +1,9 @@
+require "inspired/version"
+
+module Inspired
+  class Quotes
+    def say
+      "Great minds discuss ideas; average minds discuss events; small minds discuss people."
+    end
+  end
+end
---
+++ lib/inspired/version.rb
@@ -0,0 +1,3 @@
+module Inspired
+  VERSION = "0.1.0"
+end
